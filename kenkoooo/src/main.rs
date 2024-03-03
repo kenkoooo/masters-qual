51178230cd97dd60172bac261d3aa5dd426cb4c9
kenkoooo <kenkou.n@gmail.com>
2024-03-03 15:37:30 +0900

    gen

--- kenkoooo/src/main.rs
+++ kenkoooo/src/main.rs
@@ -7,13 +7,11 @@ const R: usize = 1;
 const D: usize = 2;
 const L: usize = 3;
 
-const X: [usize; 4] = [0, 1, 0, !0];
-const Y: [usize; 4] = [!0, 0, 1, 0];
+const X: [usize; 5] = [0, 1, 0, !0, 0];
+const Y: [usize; 5] = [!0, 0, 1, 0, 0];
 
 fn main() {
     let mut rng = StdRng::seed_from_u64(71);
-    let mut hash = vec![0; 10000];
-    for i in 0..10000 {}
 
     let (r, w) = (std::io::stdin(), std::io::stdout());
     let mut sc = IO::new(r.lock(), w.lock());
@@ -24,9 +22,9 @@ fn main() {
     let mut paths = vec![vec![vec![false; 4]; n]; n];
 
     for i in 0..n {
+        let s = sc.chars();
         for j in 1..n {
-            let v: usize = sc.read();
-            if v == 0 {
+            if s[j - 1] == '0' {
                 paths[i][j - 1][R] = true;
                 paths[i][j][L] = true;
             }
@@ -34,9 +32,9 @@ fn main() {
     }
 
     for i in 1..n {
+        let s = sc.chars();
         for j in 0..n {
-            let h: usize = sc.read();
-            if h == 0 {
+            if s[j] == '0' {
                 paths[i - 1][j][D] = true;
                 paths[i][j][U] = true;
             }
@@ -71,25 +69,179 @@ fn main() {
     }
 
     let mut heap = BinaryHeap::new();
+    heap.push(State {
+        board,
+        score,
+        pos1: (0, 0),
+        pos2: (n - 1, n - 1),
+        n,
+        log: vec![],
+    });
+
+    let mut next = BinaryHeap::new();
+    const SIZE: usize = 200;
+
+    let start = std::time::Instant::now();
+    for _ in 0..(4 * n * n) {
+        if start.elapsed().as_millis() > 1900 {
+            break;
+        }
+        while let Some(state) = heap.pop() {
+            for swap in 0..2 {
+                for move1 in 0..5 {
+                    let (i1, j1) = state.pos1;
+                    if move1 < 4 && !paths[i1][j1][move1] {
+                        continue;
+                    }
+
+                    let ni1 = i1.wrapping_add(X[move1]);
+                    let nj1 = j1.wrapping_add(Y[move1]);
+                    if ni1 >= state.n || nj1 >= state.n {
+                        continue;
+                    }
+
+                    for move2 in 0..5 {
+                        let (i2, j2) = state.pos2;
+                        if move2 < 4 && !paths[i2][j2][move2] {
+                            continue;
+                        }
+
+                        let ni2 = i2.wrapping_add(X[move2]);
+                        let nj2 = j2.wrapping_add(Y[move2]);
+                        if ni2 >= state.n || nj2 >= state.n {
+                            continue;
+                        }
+
+                        if move1 == 4 && move2 == 4 {
+                            continue;
+                        }
+
+                        let mut state = state.clone();
+                        if swap == 1 {
+                            state.swap();
+                        }
+                        state.log.push((swap, move1, move2));
+                        state.pos1 = (ni1, nj1);
+                        state.pos2 = (ni2, nj2);
+
+                        next.push(state);
+                    }
+                }
+            }
+        }
+
+        while let Some(state) = next.pop() {
+            heap.push(state);
+            if heap.len() == SIZE {
+                break;
+            }
+        }
+        next.clear();
+    }
+
+    let state = heap.pop().unwrap();
+    sc.write(format!("0 0 {} {}\n", n - 1, n - 1));
+    for (swap, move1, move2) in state.log {
+        let move1 = match move1 {
+            0 => 'U',
+            1 => 'R',
+            2 => 'D',
+            3 => 'L',
+            4 => '.',
+            _ => unreachable!(),
+        };
+        let move2 = match move2 {
+            0 => 'U',
+            1 => 'R',
+            2 => 'D',
+            3 => 'L',
+            4 => '.',
+            _ => unreachable!(),
+        };
+
+        sc.write(format!("{} {} {}\n", swap, move1, move2));
+    }
+}
+
+impl State {
+    fn swap(&mut self) {
+        let (i1, j1) = self.pos1;
+        let (i2, j2) = self.pos2;
+        let a1 = self.board[i1 * self.n + j1];
+        let a2 = self.board[i2 * self.n + j2];
+
+        self.pull(i1, j1);
+        self.push(i1, j1, a2);
+
+        self.pull(i2, j2);
+        self.push(i2, j2, a1);
+    }
+
+    fn pull(&mut self, i: usize, j: usize) {
+        let mut remove = 0;
+        let a = self.board[i * self.n + j];
+        for d in 0..4 {
+            let ni = i.wrapping_add(X[d]);
+            let nj = j.wrapping_add(Y[d]);
+            if ni >= self.n || nj >= self.n {
+                continue;
+            }
+
+            let b = self.board[ni * self.n + nj];
+            let da = a - b;
+            remove += da * da;
+        }
+
+        self.score -= remove;
+        self.board[i * self.n + j] = 0;
+    }
+
+    fn push(&mut self, i: usize, j: usize, a: i64) {
+        let mut add = 0;
+        self.board[i * self.n + j] = a;
+        for d in 0..4 {
+            let ni = i.wrapping_add(X[d]);
+            let nj = j.wrapping_add(Y[d]);
+            if ni >= self.n || nj >= self.n {
+                continue;
+            }
+
+            let b = self.board[ni * self.n + nj];
+            let da = a - b;
+            add += da * da;
+        }
+
+        self.score += add;
+    }
 }
 
 #[derive(Clone)]
 struct State {
-    board: Vec<Vec<i64>>,
+    board: Vec<i64>,
     score: i64,
     pos1: (usize, usize),
     pos2: (usize, usize),
+    n: usize,
+    log: Vec<(usize, usize, usize)>,
+}
+
+impl Eq for State {}
+
+impl Ord for State {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.partial_cmp(other).unwrap()
+    }
 }
 
 impl PartialEq for State {
     fn eq(&self, other: &Self) -> bool {
-        self.score == other.score
+        false
     }
 }
 
 impl PartialOrd for State {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(self.score.cmp(&other.score))
+        Some(other.score.cmp(&self.score))
     }
 }
 
